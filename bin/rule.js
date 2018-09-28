c09de422f07797fe3f4f1e178bbe1af2c3b5e665
yanweiqing <yanweiqing@meicai.cn>
2018-09-28 09:02:41 +0800

    修改template位置到原仓库，减少依赖

--- bin/rule.js
+++ bin/rule.js
@@ -2,14 +2,19 @@ var shell = require('shelljs');
 var colors = require('colors');
 var path = require('path');
 
+var currentPath = process.cwd();
+
 function create() {
-  shell.cp('-Rf', path.resolve(__dirname, '../template/*'), '.');
+  shell.cp('-Rf', currentPath + '/node_modules/@mc/fe-server/template/*', '.');
   console.log('\n----------> mc-server 初始化成功\n'.green);
 }
 
 function install() {
-  if (shell.exec('npm --registry=https://registrymnpm.stage.yunshanmeicai.com install @mc/fe-server@0.1.0 --save-dev', { silent: false }).code === 0) {
+  // 版本号暂时写死
+  if (shell.exec('npm --registry=https://registrymnpm.stage.yunshanmeicai.com install @mc/fe-server@latest --save-dev', { silent: false }).code === 0) {
     console.log('\n----------> @mc/fe-server安装成功\n'.green);
+    // 暂时没有分库，引用路径区分
+    console.log('\n----------> 请手动修改MCServer引用路径\n'.yellow);
   }
 }
 
--- template/server/middlewares/user-defined.js
+++
@@ -1,7 +0,0 @@
-// 自定义中间件
-export default async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`User defined log: ${ctx.method} ${ctx.url} - ${ms}ms`)
-}
