29a0ff2b7bb76d322dcecc1a7948273bdcab1170
张子元 <zhangziyuand@gmail.com>
2018-09-29 21:30:47 +0800

    feat: 调整代码结构，将脚本放入bin

--- bin/index.js
+++ bin/index.js
@@ -2,8 +2,8 @@
 var program = require('commander');
 var colors = require('colors');
 
-var package = require('./package.json');
-var rule = require('./bin/rule.js');
+var package = require('../package.json');
+var utils = require('./utils.js');
 
 program
   .version(package.version)
@@ -13,21 +13,21 @@ program
   .command('install')
   .description('安装依赖')
   .action((options) => {
-    rule.install();
+    utils.install();
   });
 
 program
   .command('init')
   .description('生成初始化文件')
   .action((options) => {
-    rule.create();
+    utils.create();
   });
 
 program
   .command('help')
   .description('操作指南')
   .action((options) => {
-    rule.help();
+    utils.help();
   })
 
 program
@@ -36,7 +36,7 @@ program
   .option('-D, --deploy [value]', '修改deploy状态，dev or prod')
   .option('-N, --node [value]', '修改环境状态，test or stage or production')
   .action((options) => {
-    rule.run(options);
+    utils.run(options);
   })
 
 program
---
+++ bin/utils.js
@@ -0,0 +1,43 @@
+var shell = require('shelljs');
+var colors = require('colors');
+var path = require('path');
+require('shelljs-plugin-open');
+
+var currentPath = process.cwd();
+
+function create() {
+  shell.cp('-Rf', currentPath + '/node_modules/@mc/fe-server/template', './service');
+  console.log('\n> mc-server 初始化成功\n'.green);
+    // 暂时没有分库，引用路径区分
+  console.log('\n> 请手动修改MCServer引用路径\n'.yellow);
+  console.log('\n> 请手动修改pm2.json 以及 agenthub.json 中的项目配置\n'.yellow);
+  console.log('\n> 请参考views下layout格式为自己的模版文件\n'.yellow);
+}
+
+function install() {
+  // 版本号暂时写死
+  if (shell.exec('npm --registry=https://registrymnpm.stage.yunshanmeicai.com install @mc/fe-server@latest --save-dev').code === 0) {
+    console.log('\n> @mc/fe-server安装成功\n'.green);
+  }
+}
+
+function help() {
+  shell.open('https://mnpm.stage.yunshanmeicai.com/package/@mc/fe-server-cli');
+}
+
+function run(options) {
+  var deployEnv = options.deploy ? options.deploy : 'dev';
+  var nodeEnv = options.node ? options.node : 'test';
+
+  if (deployEnv === 'prod') {
+    shell.exec(`export NODE_ENV=${nodeEnv} && pm2 start config/pm2.json`);
+  
+  } else {
+    shell.exec(`export NODE_ENV=${nodeEnv} && nodemon index.js`);
+  
+  }
+}
+
+module.exports = {
+  create, install, help, run
+};
